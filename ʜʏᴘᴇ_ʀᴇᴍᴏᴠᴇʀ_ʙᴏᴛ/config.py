e575ae3444b8f3d792c1fbb5e4f34ad7f9968a26
hypevoidsoul <hypevoidsoul@yahoo.com>
2021-07-09 07:28:38 -0700

    DONE!!!!

--- ʜʏᴘᴇ_ʀᴇᴍᴏᴠᴇʀ_ʙᴏᴛ/config.py
+++ ʜʏᴘᴇ_ʀᴇᴍᴏᴠᴇʀ_ʙᴏᴛ/config.py
@@ -3,7 +3,7 @@ class Config(object):
     FEEDBACK=True
     API_ID=6372795
     API_HASH ="4b7731b0a6d8e15bef82863887feb293"
-    TOKEN="1805926665:AAFDdRkVxSLF_iWyy05MHz2pZ3Sv082x7xg"
+    TOKEN="1861790845:AAFmfVGvgoEadA_JGwotw5XpmSH6BEpiE6Y"
     LOAD=[]
     WORKERS=8  
 
